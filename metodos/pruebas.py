9d069d307f655d3cd2a9525375851bd10b549388
Elra545 <159407307+Elra545@users.noreply.github.com>
2024-02-07 23:53:25 -0500

    Prueba de Sentencia

---
+++ metodos/pruebas.py
@@ -0,0 +1,7 @@
+a = int(input("poner un numero:")) #primera sentencia   
+b = int(input("poner un numero:")) #segunda sentencia
+c = a+b #tercera sentencia
+if c > 10: # caurta sentecnia
+    print(c) #quinta sentencia
+else: #sexta sentencia
+    pass
